945206cba4a0e16ae8b25f4fd94eb9a4abbbb194
Gallaann <reindzer26rus@gmail.com>
2023-04-06 02:23:23 +0300

    deleted unnecessary spacings between methods of class

--- include/title_by_character_finder.h
+++ include/title_by_character_finder.h
@@ -13,19 +13,15 @@
 class MovieTitles {
 public:
     bool ParseID(const std::filesystem::path &path, const std::string &character_name);
-
     bool ParsePrimaryTitle(const std::filesystem::path &path);
-
     bool ParseLocalizedTitle(const std::filesystem::path &path);
-
     void PrintResult() const;
-
     bool static findColumns(std::string &columns_naming_line, std::unordered_map<std::string, int> &column_names_map);
 
 private:
-    std::unordered_map<std::string, std::string> m_titles;
-
     bool static getInputStream(std::stringstream &input_stream, const std::filesystem::path &path_to_file);
+
+    std::unordered_map<std::string, std::string> m_titles;
 };
 
 #endif //TASK1_TITLE_BY_CHARACTER_FINDER_H
